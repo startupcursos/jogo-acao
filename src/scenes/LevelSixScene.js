078b3937e25d3838025075628dd65cdb529ff494
fabianoluxsantos <dreamerguardian@yahoo.com.br>
2014-06-02 20:06:40 -0300

    14-Projeto-Implementacao-Fase-4-5-e-6 closes #14

---
+++ src/scenes/LevelSixScene.js
@@ -0,0 +1,36 @@
+var LevelSixScene = cc.Scene.extend({
+	player: null,
+	gamelayer: null,
+	canvas: null,
+	onEnter : function() {
+		this._super();
+		this.canvas = cc.Director.getInstance().getWinSize();
+
+		var layerLandscape2 = new Landscape2Layer();
+		this.addChild(layerLandscape2, layerLandscape2.zOrder);
+		layerLandscape2.init();
+		
+		var layerLandscape1 = new Landscape1Layer();
+		this.addChild(layerLandscape1, layerLandscape1.zOrder);
+		layerLandscape1.init();
+		
+		this.gamelayer = new GameLayer();
+		this.addChild(this.gamelayer, this.gamelayer.zOrder);
+		this.gamelayer.init();
+		this.enemyPlacement();
+		this.player = this.gamelayer.player;
+	},
+	enemyPlacement : function() {
+		this.gamelayer.addChild(new UfoC(700, 5 / 6 * this.canvas.height));
+		this.gamelayer.addChild(new UfoC(1000, 5 / 6 * this.canvas.height));		
+		this.gamelayer.addChild(new UfoB(3800, 5 / 6 * this.canvas.height));
+		this.gamelayer.addChild(new UfoB(4100, 5 / 6 * this.canvas.height));
+		this.gamelayer.addChild(new HoleSmall(6000, this.canvas.height / 6.5));
+		this.gamelayer.addChild(new Stone(8000, this.canvas.height / 3.5));
+						
+	},
+	levelFinished : function() {
+		cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1, new LevelSevenScene()));
+	}
+
+});
