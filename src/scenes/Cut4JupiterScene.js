382307987076ad8d999955d02cb63711960e11be
Andre Bechara <andre.bechara@startupcursos.com.br>
2014-07-09 16:08:06 -0300

    Jupiter CutScene

--- src/scenes/Cut4JupiterScene.js
+++ src/scenes/Cut4JupiterScene.js
@@ -2,18 +2,27 @@ var Cut4JupiterScene = cc.Scene.extend({
 	onEnter : function() {
 		this._super();
 		var size = cc.Director.getInstance().getWinSize();        
+
 		var imagemDoPlaneta = cc.MenuItemImage.create(s_jupiter_cut_image, s_jupiter_cut_image, function() {
 			cc.Director.getInstance().replaceScene(cc.TransitionFade.create(1, new Level4JupiterScene()));
 		}, this);
 		imagemDoPlaneta.setPosition(size.width / 2, size.height / 2);
-        var menu = cc.Menu.create(imagemDoPlaneta);
+
+		cc.MenuItemFont.setFontName("Impact");
+		cc.MenuItemFont.setFontSize(48);
+        var play = cc.MenuItemFont.create("GO");
+        play.setPosition(size.width / 2, size.height / 5);
+        
+        var menu = cc.Menu.create(imagemDoPlaneta, play);
+        
         menu.setPosition(0, 0);
         this.addChild(menu, 1);
-        var nomeDoPlaneta = cc.LabelTTF.create("JUPITER", "Impact", 38);
+        var nomeDoPlaneta = cc.LabelTTF.create("JUPITER", "Impact", 48);
         nomeDoPlaneta.setPosition(size.width / 2, size.height * 3 / 4);
         this.addChild(nomeDoPlaneta, 1);
-        var clickToPlay = cc.LabelTTF.create("Click to Play", "Impact", 38);
-        clickToPlay.setPosition(size.width / 2, size.height * 1 / 4);
+        var clickToPlay = cc.LabelTTF.create("Andre Bechara - Level Design\nAndre Bechara - Landscape Art", "Arial", 24);
+        clickToPlay.setPosition(size.width / 2, size.height / 2);
         this.addChild(clickToPlay, 1);
+        
 	}
 });
