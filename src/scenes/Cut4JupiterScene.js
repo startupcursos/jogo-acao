6f122530f30f73d97f2aa6f7be8b5a587cf63396
Andre Bechara <andre.bechara@startupcursos.com.br>
2014-07-09 19:23:04 -0300

    Cutscene

--- src/scenes/Cut4JupiterScene.js
+++ src/scenes/Cut4JupiterScene.js
@@ -3,6 +3,10 @@ var Cut4JupiterScene = cc.Scene.extend({
 		this._super();
 		var size = cc.Director.getInstance().getWinSize();
 
+		var nomeDoPlaneta = cc.LabelTTF.create("JUPITER", "Impact", 48);
+		nomeDoPlaneta.setPosition(size.width / 2, size.height * 3 / 4);
+		this.addChild(nomeDoPlaneta, 1);
+		
 		cc.MenuItemFont.setFontName("Impact");
 		cc.MenuItemFont.setFontSize(48);
 		var go = cc.MenuItemFont.create("GO", this.onGo);
@@ -11,9 +15,6 @@ var Cut4JupiterScene = cc.Scene.extend({
 		menu.setPosition(0, 0);
 		this.addChild(menu, 1);
 
-		var nomeDoPlaneta = cc.LabelTTF.create("JUPITER", "Impact", 48);
-		nomeDoPlaneta.setPosition(size.width / 2, size.height * 3 / 4);
-		this.addChild(nomeDoPlaneta, 1);
 		var clickToPlay = cc.LabelTTF.create("Andre Bechara - Level Design\nAndre Bechara - Landscape Art", "Arial", 24);
 		clickToPlay.setPosition(size.width / 2, size.height / 2);
 		this.addChild(clickToPlay, 1);
