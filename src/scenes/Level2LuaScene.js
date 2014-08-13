3897dcc67227671307f495a792792d37c5d6f050
macspecial <marcelloabtibol@hotmail.com>
2014-08-13 20:29:55 -0300

    level_uranus

--- src/scenes/Level2LuaScene.js
+++ src/scenes/Level2LuaScene.js
@@ -18,7 +18,7 @@ var Level2LuaScene = cc.Scene.extend({
 		this.addChild(layerMontanhaPerto, layerMontanhaPerto.zOrder);
 		layerMontanhaPerto.init();
 
-			this.gamelayer = new GameLayer();
+		this.gamelayer = new GameLayer();
 		this.addChild(this.gamelayer, this.gamelayer.zOrder);
 		var spriteGround = new Ground(0, 0, s_lua_chao);
 		this.gamelayer.init(spriteGround,  s_bgm_lua);
@@ -28,13 +28,13 @@ var Level2LuaScene = cc.Scene.extend({
 		this.enemyPlacement();
 		this.player = this.gamelayer.player;
 		
-			var hudLayer = new HudLayer();
+		var hudLayer = new HudLayer();
 		hudLayer.init(230,176,46);
 		this.addChild(hudLayer, hudLayer.zOrder);
 		GAME.LASTLEVEL = new Level2LuaScene();
 	},
 	enemyPlacement : function() {
-		this.gamelayer.addChild(new Tank(6000, this.canvas.height / 4));
+		this.gamelayer.addChild(new Tank(6000, this.canvas.height / 11));
 		this.gamelayer.addChild(new NaveSeguidora(-500, this.canvas.height / 3.5));
 		this.gamelayer.addChild(new UfoA(800,  5 / 6 * this.canvas.height));
 		this.gamelayer.addChild(new UfoB(1200,  5 / 6 * this.canvas.height));
@@ -43,8 +43,6 @@ var Level2LuaScene = cc.Scene.extend({
 		this.gamelayer.addChild(new BigStone(3200, this.canvas.height / 3.3));
 		this.gamelayer.addChild(new SmallRollingStone(10000, this.canvas.height / 4));
 		this.gamelayer.addChild(new RollingStone(11000, this.canvas.height / 3.5));
-		this.gamelayer.addChild(new HoleSmall(4000, this.canvas.height / 6.5));
-		this.gamelayer.addChild(new HoleBig(4800, this.canvas.height / 6.5));
 		this.gamelayer.addChild(new Mina(5600, this.canvas.height / 4.5));
 		this.gamelayer.addChild(new Planta(6400, this.canvas.height / 6.5));
 	},
